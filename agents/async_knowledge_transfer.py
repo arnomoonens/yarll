fd7acd9cfc300c2a97a643ecf3abd41f5f0dbb94
Arno Moonens <x-006@hotmail.com>
2017-05-15 12:12:53 +0200

    Use a different optimizer for each task in AKT

--- agents/async_knowledge_transfer.py
+++ agents/async_knowledge_transfer.py
@@ -18,6 +18,7 @@ class AKTThread(Thread):
     def __init__(self, master, env, task_id, n_iter, start_at_iter=0):
         super(AKTThread, self).__init__()
         self.master = master
+        self.config = self.master.config
         self.task_id = task_id
         self.n_iter = n_iter
         self.start_at_iter = start_at_iter
@@ -30,6 +31,8 @@ class AKTThread(Thread):
 
         # Write the summary of each task in a different directory
         self.writer = tf.summary.FileWriter(os.path.join(self.master.monitor_path, "task" + str(self.task_id)), self.master.session.graph)
+
+        self.optimizer = tf.train.RMSPropOptimizer(learning_rate=self.config["learning_rate"], decay=self.config["decay"], epsilon=self.config["epsilon"])
 
     def build_networks(self):
         with tf.variable_scope("task{}".format(self.task_id)):
@@ -52,7 +55,6 @@ class AKTThread(Thread):
     def learn_REINFORCE(self):
         """Learn using updates like in the REINFORCE algorithm."""
         reporter = Reporter()
-        config = self.master.config
         total_n_trajectories = 0
         iteration = self.start_at_iter
         while iteration < self.n_iter and not self.master.stop_requested:
@@ -62,7 +64,7 @@ class AKTThread(Thread):
             total_n_trajectories += len(trajectories)
             all_state = np.concatenate([trajectory["state"] for trajectory in trajectories])
             # Compute discounted sums of rewards
-            rets = [discount_rewards(trajectory["reward"], config["gamma"]) for trajectory in trajectories]
+            rets = [discount_rewards(trajectory["reward"], self.config["gamma"]) for trajectory in trajectories]
             max_len = max(len(ret) for ret in rets)
             padded_rets = [np.concatenate([ret, np.zeros(max_len - len(ret))]) for ret in rets]
             # Compute time-dependent baseline
@@ -81,18 +83,16 @@ class AKTThread(Thread):
             })
             print("Task:", self.task_id)
             reporter.print_iteration_stats(iteration, episode_rewards, episode_lengths, total_n_trajectories)
-            results = self.master.session.run([self.master.summary_op], feed_dict={
+            summary = self.master.session.run([self.master.summary_op], feed_dict={
                 self.master.loss: results[0],
                 self.master.reward: np.mean(episode_rewards),
                 self.master.episode_length: np.mean(episode_lengths)
             })
-            self.writer.add_summary(results[0], iteration)
+            self.writer.add_summary(summary[0], iteration)
             self.writer.flush()
 
     def learn_Karpathy(self):
         """Learn using updates like in the Karpathy algorithm."""
-        config = self.master.config
-
         iteration = self.start_at_iter
         while iteration < self.n_iter and not self.master.stop_requested:  # Keep executing episodes until the master requests a stop (e.g. using SIGINT)
             iteration += 1
@@ -100,7 +100,7 @@ class AKTThread(Thread):
             reward = sum(trajectory["reward"])
             action_taken = trajectory["action"]
 
-            discounted_episode_rewards = discount_rewards(trajectory["reward"], config["gamma"])
+            discounted_episode_rewards = discount_rewards(trajectory["reward"], self.config["gamma"])
             # standardize
             discounted_episode_rewards -= np.mean(discounted_episode_rewards)
             std = np.std(discounted_episode_rewards)
@@ -154,6 +154,7 @@ class AsyncKnowledgeTransfer(Agent):
             log_device_placement=False,
             allow_soft_placement=True))
 
+        self.global_step = tf.get_variable("global_step", [], tf.int32, initializer=tf.constant_initializer(0, dtype=tf.int32), trainable=False)
         self.build_networks()
 
         self.loss = tf.placeholder("float", name="loss")
@@ -176,11 +177,13 @@ class AsyncKnowledgeTransfer(Agent):
         for i, job in enumerate(self.jobs):
             only_sparse = (self.config["switch_at_iter"] is not None and i == len(self.jobs) - 1)
             grads = tf.gradients(job.loss, (self.shared_vars if not(only_sparse) else []) + [job.sparse_representation])
-            print(i)
-            job.apply_grad = self.optimizer.apply_gradients(zip(
-                grads,
-                (self.shared_vars if not(only_sparse) else []) + [job.sparse_representation]
-            ))
+            job.apply_grad = job.optimizer.apply_gradients(
+                zip(
+                    grads,
+                    (self.shared_vars if not(only_sparse) else []) + [job.sparse_representation]
+                ),
+                global_step=self.global_step
+            )
 
         self.session.run(tf.global_variables_initializer())
 
@@ -210,8 +213,6 @@ class AsyncKnowledgeTransfer(Agent):
             self.knowledge_base = tf.Variable(tf.random_normal([self.nO, self.config["n_sparse_units"]]), name="knowledge_base")
 
             self.shared_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, tf.get_variable_scope().name)
-
-            self.optimizer = tf.train.RMSPropOptimizer(learning_rate=self.config["learning_rate"], decay=self.config["decay"], epsilon=self.config["epsilon"])
 
     def signal_handler(self, signal, frame):
         """When a (SIGINT) signal is received, request the threads (via the master) to stop after completing an iteration."""

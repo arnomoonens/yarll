c63759e79885b269c13a116fca5026bbb8fb79b1
Arno Moonens <arno.moonens@gmail.com>
2019-11-19 14:10:00 +0100

    Version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="yarll",
-    version="0.0.11",
+    version="0.0.12",
     author="Arno Moonens",
     author_email="arno.moonens@outlook.com",
     description="Yet Another Reinforcement Learning Library",

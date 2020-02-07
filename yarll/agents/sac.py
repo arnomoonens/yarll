a55dca4bd300fa6f89db846c69c269533778ee16
Arno Moonens <arno.moonens@gmail.com>
2020-02-07 22:14:27 +0100

    SAC: Use EnvRunner

--- yarll/agents/sac.py
+++ yarll/agents/sac.py
@@ -8,8 +8,10 @@ import tensorflow_addons as tfa
 import tensorflow_probability as tfp
 
 from yarll.agents.agent import Agent
+from yarll.agents.env_runner import EnvRunner
 from yarll.memory.memory import Memory
 from yarll.misc.utils import hard_update, soft_update
+
 
 class SAC(Agent):
     def __init__(self, env, monitor_path: str, **usercfg) -> None:
@@ -19,7 +21,7 @@ class SAC(Agent):
 
         self.config.update(
             n_episodes=100000,
-            n_timesteps=env.spec.tags.get("wrapper_config.TimeLimit.max_episode_steps"),
+            n_timesteps=env.spec.max_episode_steps,
             actor_learning_rate=3e-4,
             softq_learning_rate=3e-4,
             value_learning_rate=3e-4,
@@ -28,9 +30,9 @@ class SAC(Agent):
             batch_size=128,
             tau=0.01,
             n_actor_layers=2,
-            logprob_epsilon=1e-6, # For numerical stability when computing tf.log
+            logprob_epsilon=1e-6,  # For numerical stability when computing tf.log
             n_hidden_units=128,
-            n_train_steps=4, # Number of parameter update steps per iteration
+            n_train_steps=4,  # Number of parameter update steps per iteration
             replay_buffer_size=1e6,
             replay_start_size=128  # Required number of replay buffer entries to start training
         )
@@ -38,6 +40,8 @@ class SAC(Agent):
 
         self.state_shape: list = list(env.observation_space.shape)
         self.n_actions: int = env.action_space.shape[0]
+        self.action_low = self.env.action_space.low
+        self.action_high = self.env.action_space.high
 
         # Make networks
         # action_output are the squashed actions and action_original those straight from the normal distribution
@@ -64,6 +68,12 @@ class SAC(Agent):
         self.total_steps = 0
         self.total_episodes = 0
         self.writer = tf.summary.create_file_writer(str(self.monitor_path))
+
+        self.env_runner = EnvRunner(self.env,
+                                    self,
+                                    usercfg,
+                                    normalize_states=False,
+                                    summary_writer=self.writer)
 
     def value(self, states: np.ndarray) -> np.ndarray:
         return self.value_network(states)
@@ -116,8 +126,6 @@ class SAC(Agent):
         return softq_mean[0], tf.sqrt(softq_variance[0]), softq_loss, actor_loss, value_loss, tf.reduce_mean(action_logprob)
 
     def learn(self):
-        action_low = self.env.action_space.low
-        action_high = self.env.action_space.high
         # Arrays to keep results from train function over different train steps in
         softq_means = np.empty((self.config["n_train_steps"],), np.float32)
         softq_stds = np.empty((self.config["n_train_steps"],), np.float32)
@@ -127,24 +135,17 @@ class SAC(Agent):
         action_logprob_means = np.empty((self.config["n_train_steps"],), np.float32)
         with self.writer.as_default():
             for _ in range(self.config["n_episodes"]):
-                state = self.env.reset().astype(np.float32)
-                episode_reward = 0
-                episode_length = 0
                 for _ in range(self.config["n_timesteps"]):
-                    action = self.action(state)
-                    to_execute = action_low + (action + 1.0) * 0.5 * (action_high - action_low)
-                    new_state, reward, done, _ = self.env.step(to_execute)
-                    new_state = new_state.astype(np.float32)
-                    episode_length += 1
-                    self.total_steps += 1
-                    episode_reward += reward
-                    self.replay_buffer.add(state, action, reward, new_state, done)
+                    experience = self.env_runner.get_steps(1)[0]
+                    self.replay_buffer.add(experience.state, experience.action, experience.reward,
+                                           experience.next_state, experience.terminal)
                     if self.replay_buffer.n_entries > self.config["replay_start_size"]:
                         for i in range(self.config["n_train_steps"]):
                             sample = self.replay_buffer.get_batch(self.config["batch_size"])
                             softq_mean, softq_std, softq_loss, actor_loss, value_loss, action_logprob_mean = self.train(
                                 sample["states0"],
-                                np.resize(sample["actions"], [self.config["batch_size"], self.n_actions]),  # for n_actions == 1
+                                np.resize(sample["actions"], [self.config["batch_size"],
+                                                              self.n_actions]),  # for n_actions == 1
                                 sample["rewards"],
                                 sample["states1"],
                                 sample["terminals1"])
@@ -161,15 +162,18 @@ class SAC(Agent):
                         tf.summary.scalar("model/value_loss", np.mean(value_losses), self.total_steps)
                         tf.summary.scalar("model/action_logprob_mean", np.mean(action_logprob_means), self.total_steps)
                         # Update the target networks
-                        soft_update(self.value_network.variables, self.target_value_network.variables, self.config["tau"])
+                        soft_update(self.value_network.variables,
+                                    self.target_value_network.variables, self.config["tau"])
                         self.n_updates += 1
-                    state = new_state
-                    if done:
-                        self.total_episodes += 1
-                        tf.summary.scalar("env/Episode_length", episode_length, self.total_steps)
-                        tf.summary.scalar("env/Reward", episode_reward, self.total_steps)
-                        tf.summary.scalar("env/N_episodes", self.total_episodes, step=self.total_steps)
+                    if experience.terminal:
                         break
+
+    def choose_action(self, state, features):
+        return {"action": self.action(state)}
+
+    def get_env_action(self, action):
+        return self.action_low + (action + 1.0) * 0.5 * (self.action_high - self.action_low)
+
 
 class ActorNetwork(Model):
     def __init__(self, n_hidden_layers, n_hidden_units, n_actions, logprob_epsilon):
@@ -199,6 +203,7 @@ class ActorNetwork(Model):
             tf.math.log(1.0 - tf.pow(squashed_actions, 2) + self.logprob_epsilon)
         logprob = tf.reduce_sum(logprob, axis=-1, keepdims=True)
         return squashed_actions, logprob
+
 
 class SoftQNetwork(Model):
     def __init__(self, n_hidden_layers, n_hidden_units):
